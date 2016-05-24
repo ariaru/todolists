c358ad5408af1301431bcf699447f624389f6a1a
ariaru <ariaru@hotmail.de>
2016-05-25 00:51:40 +0200

    has-many-through association between User and TodoItem via TodoList

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
 	has_one :profile, dependent: :destroy
 	has_many :todo_lists, dependent: :destroy
+	# Assignments specifies to use :source option even though the name of the
+	# source association can be automatically inferred from the association name
+	has_many :todo_items, through: :todo_lists, source: :todo_items
 end
